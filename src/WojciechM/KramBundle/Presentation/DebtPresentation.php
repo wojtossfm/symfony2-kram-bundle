471d9106f2ccefd0200b08b88a2f834f7ddee905
wojciechm <wojciechm-HP-Compaq-nx7400-RH640ES-AKD>
2014-08-12 17:25:10 +0200

    Finished working on basic presentation classes for now. Finished reworking basic template logic for crud views.

---
+++ src/WojciechM/KramBundle/Presentation/DebtPresentation.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace WojciechM\KramBundle\Presentation;
+class DebtPresentation extends Presentation {
+	
+	public function __construct() {
+		$this->fields = array(
+			new PresentationField(PresentationField::$TYPE_SINGLE, "user", "User", NULL),
+			new PresentationField(PresentationField::$TYPE_SINGLE, "week", "Week", NULL),
+			new PresentationField(PresentationField::$TYPE_SINGLE, "amount", "Amount", NULL),
+			new PresentationField(PresentationField::$TYPE_SINGLE, "dateCreated", "Created", PresentationField::$FORMAT_DATE),
+		);
+		$this->rowActions = array("edit" => "debt_edit", "delete" => "debt_delete");
+		$this->labels = array('plural' => 'Debts', 'single' => 'Debt');
+		$this->generalActions = array("new" => "debt_new");
+	}
+
+}
---
+++ src/WojciechM/KramBundle/Presentation/ExpensePresentation.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace WojciechM\KramBundle\Presentation;
+class ExpensePresentation extends Presentation {
+	
+	public function __construct() {
+		$this->fields = array(
+			new PresentationField(PresentationField::$TYPE_SINGLE, "week", "Week", NULL),
+			new PresentationField(PresentationField::$TYPE_SINGLE, "amount", "Amount", NULL),
+			new PresentationField(PresentationField::$TYPE_SINGLE, "dateCreated", "Created", PresentationField::$FORMAT_DATE),
+			new PresentationField(PresentationField::$TYPE_SINGLE, "comment", "Comment", NULL),
+		);
+		$this->rowActions = array("edit" => "expense_edit", "delete" => "expense_delete");
+		$this->labels = array('plural' => 'Expenses', 'single' => 'Expense');
+		$this->generalActions = array("new" => "expense_new");
+	}
+
+}
